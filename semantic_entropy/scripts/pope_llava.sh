b2bca539ccdfb57d0130f05de50397302f202d09
421zuoduan <ruochencui421@gmail.com>
2025-02-25 21:21:24 +0800

    run 300 samples

--- semantic_entropy/scripts/pope_llava.sh
+++ semantic_entropy/scripts/pope_llava.sh
@@ -1,14 +1,29 @@
-# 测试15样本能不能跑
+# 测试 150 样本时语义熵和朴素熵的情况
 python -m semantic_entropy.model_vqa_loader \
     --model-path /home/wuzongqian/model/llava-v1.5-7b \
-    --question-file ./playground/data/eval/pope/llava_pope_test_15.jsonl \
+    --question-file ./playground/data/eval/pope/llava_pope_test_300.jsonl \
     --image-folder ./playground/data/eval/pope/val2014 \
     --answers-file ./playground/data/eval/pope/answers/llava-v1.5-7b.jsonl \
     --greedy-search-results-file ./playground/data/eval/pope/answers/greedy_search/llava-v1.5-7b.jsonl \
     --annotation-dir ./playground/data/eval/pope/coco \
-    --samples 10\
+    --samples 5\
     --temperature 1 \
     --conv-mode vicuna_v1
+
+
+
+
+# # 测试15样本能不能跑
+# python -m semantic_entropy.model_vqa_loader \
+#     --model-path /home/wuzongqian/model/llava-v1.5-7b \
+#     --question-file ./playground/data/eval/pope/llava_pope_test_15.jsonl \
+#     --image-folder ./playground/data/eval/pope/val2014 \
+#     --answers-file ./playground/data/eval/pope/answers/llava-v1.5-7b.jsonl \
+#     --greedy-search-results-file ./playground/data/eval/pope/answers/greedy_search/llava-v1.5-7b.jsonl \
+#     --annotation-dir ./playground/data/eval/pope/coco \
+#     --samples 10\
+#     --temperature 1 \
+#     --conv-mode vicuna_v1
 
 
 # 多次采样实现, 保存pkl实现, 保存格式待检验, 本地json保存需修改, 下面要抽样本缩短时间
